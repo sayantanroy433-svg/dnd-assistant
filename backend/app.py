511ff736e678fe0e8060fcbf79d75bccd4241e30
sayantanroy433-svg <sayantan.roy433@gmail.com>
2026-06-30 17:04:35 +0530

    Add files via upload

---
+++ backend/app.py
@@ -0,0 +1,29 @@
+from rag import ask
+
+history = []
+
+print("========================================")
+print("     D&D Documentation Assistant")
+print("========================================")
+print("Type 'exit' to quit.\n")
+
+while True:
+
+    q = input("You: ")
+
+    if q.lower() in ["exit", "quit"]:
+        break
+
+    print("\nAssistant:")
+
+    answer, sources = ask(q, history)
+
+    print (answer)
+    history.append({
+        "question": q,
+        "answer": answer
+    })
+
+    history = history[-5:]
+
+    print()
